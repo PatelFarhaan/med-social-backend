c59c24ec29eec2781fe6db54c45b5e5c67ab124b
ChandrakantPal <sanjay161097@gmail.com>
2021-06-23 17:51:11 +0530

    resolver for createUpdateNotificationSettings

--- src/db/graphql/resolvers/user.js
+++ src/db/graphql/resolvers/user.js
@@ -8,7 +8,8 @@ const {
   signup,
   setPassword,
   resetPassword,
-  passwordChange
+  passwordChange,
+  updateNotificationSettings
 } = require('../../../lib/users')
 const { tokenService, emailService, socialService, stripeService, uploadService } = require('../../../lib/services')
 const { getTenantSettings } = require('../../../lib/settings')
@@ -258,7 +259,12 @@ module.exports = {
     },
     passwordChange: can(['standard', 'admin', 'superadmin']).createResolver(async (_parent, { oldPassword, newPassword }, { req }) =>
       passwordChange(req.user, oldPassword, newPassword)
-    )
+    ),
+    createUpdateUserNotificationSettings: async (_parent, args, { req }) => {
+      const { user } = req
+      const notificationSettings = await updateNotificationSettings(user.id, args)
+      return notificationSettings
+    }
   },
   User: {
     expertises: async (user, { limit = 1, page = 1 }, { db, EXPECTED_OPTIONS_KEY, context }) => {
