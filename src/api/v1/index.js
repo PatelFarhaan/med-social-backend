5e9ac2159bfd2a34773448bb0e33c53e6220cc0f
Harsha <harzkr142@gmail.com>
2021-08-16 21:18:22 +0530

    Remove test script from index

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -9,11 +9,6 @@ const SocialAuthRoutes = require('./socialAuth.route')
 module.exports = () => {
   const api = Router()
 
-  api.get('/test-email', async (_req, res) => {
-    await emailService.sendEmail('harzkr142@gmail.com', { firstName: 'Harsha', callToActionUrl: `https://joincolumn.com` }, 'waitList')
-    res.json({ status: 200 })
-  })
-
   api.use('/auth', SocialAuthRoutes)
 
   api.get('*', async (req, res, next) => {
